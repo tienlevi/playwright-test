e737026badba45f99d1d4cc0eb78b56db130ce87
tienlevi <nguyentrachtien2401@gmail.com>
2025-07-17 15:55:20 +0700

    fix: update Playwright workflow to handle screenshot paths and trigger on PR synchronize events

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -6,7 +6,7 @@ test("Screenshots", async ({ page }) => {
   await page.goto(baseLocal, { waitUntil: "networkidle" });
 
   await page.screenshot({
-    path: "public/fullpage.png",
+    path: "public/screenshots/screenshot.png",
     fullPage: true,
   });
 });
