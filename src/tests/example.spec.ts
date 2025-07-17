260ddf647dc6ed2c53096e86c35b2b61884a7ae8
tienlevi <nguyentrachtien2401@gmail.com>
2025-07-17 15:58:56 +0700

    fix: update base URL in example.spec.ts for testing

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "@playwright/test";
 
-const baseLocal = "http://localhost:5173/post/1";
+const baseLocal = "http://localhost:5173";
 
 test("Screenshots", async ({ page }) => {
   await page.goto(baseLocal, { waitUntil: "networkidle" });
