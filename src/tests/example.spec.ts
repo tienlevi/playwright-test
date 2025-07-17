044b7dcf6d398b7668002f9fcb048cd1927ee632
tienlevi <nguyentrachtien2401@gmail.com>
2025-07-17 11:10:48 +0700

    fix: update header and footer background colors to red and remove unused screenshot in tests

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -5,8 +5,6 @@ const baseLocal = "http://localhost:5173";
 test("Screenshots", async ({ page }) => {
   await page.goto(baseLocal, { waitUntil: "networkidle" });
 
-  await page.locator("header").screenshot({ path: "public/lists.png" });
-
   await page.screenshot({
     path: "public/fullpage.png",
     fullPage: true,
